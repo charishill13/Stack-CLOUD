50ae847bca05f2d4690efb6901b709fb61be92e2
Charis <charishill@outlook.com>
2021-04-17 12:27:35 -0400

    vars file for working terraform script

--- vars.tf
+++ vars.tf
@@ -1,6 +1,6 @@
 variable "AWS_ACCESS_KEY" {}
 
- variable "AWS_SECRET_KEY" {}
+variable "AWS_SECRET_KEY" {}
 
 
 variable "AWS_REGION" {
@@ -8,11 +8,11 @@ variable "AWS_REGION" {
 }
 
 variable "PATH_TO_PRIVATE_KEY" {
-  default = "mykey"
+  default = "MyEC2KeyPair"
 }
 
 variable "PATH_TO_PUBLIC_KEY" {
-  default = "mykey.pub"
+  default = "MyEc2KeyPair.pub"
 }
 
 variable "AMIS" {
@@ -24,6 +24,32 @@ variable "AMIS" {
     eu-west-1 = "ami-844e0bf7"
   }
 }
+
+variable "my_aws_subnet" {
+  type = map(string)
+  default = {
+    "us-east-1a" = "subnet-0bdf4983d2b0a4d64"
+    "us-east-1b" = "subnet-0c4d87bfb468ccf1f"
+    "us-east-1c" = "subnet-0cb81546bbc538fa4"  
+    "us-east-1d" = "subnet-0cb669c82e02d5b4c"
+    "us-east-1e" = "subnet-0bfc4104e921bbd60"
+  }
+}  
+
+variable "DATABASE_NAME" {
+  default = "stack-wordpress-db3"
+}
+variable "USERNAME" {
+  default = "wpuser"
+}
+variable "EFS_ID" {
+  default = "WordpressEFS"
+}
+variable "RDS_ENDPOINT" {
+  default = wpinstance.cth4n4flvgsw.us-east-1.rds.amazonaws.com
+} 
+#variable "INSTANCE_USERNAME" {
+#}
 
  #variable "RDS_PASSWORD" {
 # }
