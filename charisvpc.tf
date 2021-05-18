71b0dd74482833345e57b187a56929f5f8959e79
Charis <charishill@outlook.com>
2021-05-17 23:33:26 -0400

    apply

--- charisvpc.tf
+++ charisvpc.tf
@@ -163,6 +163,12 @@ resource "aws_security_group" "pubtoapp" {
     protocol         = "icmp"
     security_groups = [aws_security_group.PUBSGA.id]
   }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
  
 resource "aws_security_group" "databasesg" {
