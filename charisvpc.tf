c6000f15cb2de25e470c76dd2f2737e4e15b8c04
Charis <charishill@outlook.com>
2021-05-22 12:44:11 -0400

    dns change

--- charisvpc.tf
+++ charisvpc.tf
@@ -465,4 +465,19 @@ locals {
   )
 } */
 
+#CREATE HOSTED ZONE IN ROUTE 53
+resource "aws_route53_zone" "primary" {
+  name = "stack-charis.com"
+}
 
+resource "aws_route53_record" "www" {
+  zone_id = aws_route53_zone.primary.zone_id
+  name    = "stack-charis.com"
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.clixxapplb.dns_name
+    zone_id                = aws_lb.clixxapplb.zone_id
+    evaluate_target_health = true
+  }
+}
